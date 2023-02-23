f4b330459cbb34752d1e51afecd92671f9600445
tanvishinde03 <shindetanvi0203@gmail.com>
2023-02-23 16:24:38 +0530

    [Tanvi] Add : UC1 - Simply Adding Three Values in LinkList

---
+++ LinkedListProblem/LinkedList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    public class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into Linked List ", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+            }
+            Console.WriteLine("Linked list is: ");
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.ReadLine();
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ LinkedListProblem/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedlist = new LinkedList();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Plase enter an object an option");
+                Console.WriteLine("Select Option 1");
+                Console.WriteLine("Select Option 2");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        linkedlist.Add(56);
+                        linkedlist.Add(30);
+                        linkedlist.Add(70);
+                        break;
+                    case 2:
+                        linkedlist.Display();
+                        break;
+                    default:
+                        Console.WriteLine("Enter correct option");
+                        break;
+                }
+            }
+
+
+        }
+    }
+}
